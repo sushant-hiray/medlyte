9df1ac63a77a69cae20dba3249306a8d4b6e3e39
Anmol Garg <garganmol1993@gmail.com>
2015-03-28 18:52:29 +0530

    [E] Updated nav.html and urls.py

--- medlyte/website/urls.py
+++ medlyte/website/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from website import views
 
 urlpatterns = patterns('',
+    url(r'^create-account/$', views.create, name='create'),
     url(r'^$', views.index, name='index'),
 )
--- medlyte/website/views.py
+++ medlyte/website/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'website/index.html')
+
+def create(request):
+	return HttpResponse("I am here!")
